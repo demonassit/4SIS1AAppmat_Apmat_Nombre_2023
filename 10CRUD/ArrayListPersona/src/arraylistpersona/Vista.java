83e1fabe66197746f4a14311fb7826c48a1ce099
demonassit <jminorg@ipn.mx>
2023-10-25 07:59:46 -0600

    ya quedo wiii

--- 10CRUD/ArrayListPersona/src/arraylistpersona/Vista.java
+++ 10CRUD/ArrayListPersona/src/arraylistpersona/Vista.java
@@ -14,100 +14,102 @@ public class Vista {
     
     //vamos a crear nuestro menu
 
-    public void menu(){
+    
 
         Scanner entrada = new Scanner(System.in);
-
+        int opcion;
         //vamos a crear una inscancia de Persona y del controlador
         AccionesPersona control = new AccionesPersona();
 
-        System.out.println("Bienvenido al programa de registro de dormiditos de UTC");
-        System.out.println("Elija una opcion");
-        System.out.println("1.- Mostrar a todos los dormiditos");
-        System.out.println("2.- Registrar un nuevo dormidito");
-        System.out.println("3.- Editar a un dormidito");
-        System.out.println("4.- Despertar al dormidito");
-        System.out.println("5.- Salir");
-
-        int opcion = entrada.nextInt();
-
-        switch(opcion){
+       public void menu(){
+        do{
+        System.out.println("Bienvenido a la administracion del personal de la empresa Patito");
+        System.out.println("Por favor elija la opcion deseada"
+                + "\n 1.- Mostrar lista del personal"
+                + "\n 2.- Registrar una nueva persona"
+                + "\n 3.- Editar los datos de una persona"
+                + "\n 4.- Borrar el registro de una persona"
+                + "\n 5.- Salir");
+        
+        opcion = Integer.parseInt(entrada.nextLine());
+        
+        switch (opcion) {
             case 1:
-                //mostrar todo el registro de personas
-                System.out.println("mimidos:");
-                //es una intancia de la lista de personas
-                ArrayList<Persona> listadepersonas = control.mostrarPersonas();
-
-                //al tener la lista de personas hace falta recorrer los registros de dicha lista
-                for(Persona objeto : listadepersonas){
-                    System.out.println("El id es :" + objeto.getId() + "\n"
-                                        + "EL nombre es : " + objeto.getNombre()+ "\n"
-                                        + "La edad es : " + objeto.getEdad()+ "\n" );
+                //aqui voy a mandar a llamar al metodo de mostrarPersonas
+                //lo primero que necesito es el objeto capaz de almacenar a todas las personas
+                ArrayList<Persona> listadePersonas = control.mostrarPersonas();
+                //ahora necesito recorrer la lista
+                for(Persona objeto : listadePersonas){
+                    //imprimir sus datos
+                    System.out.println("El id: " + objeto.getId()+ "\n"
+                                        + "El nombre: " + objeto.getNombre()+ "\n"
+                                        + "La edad es: " + objeto.getEdad() + "\n");
                 }
-                menu();
+                
                 break;
             case 2:
-                //debemos registrar los datos de la persona
-                System.out.println("Ingrese el id del dormidito: ");
-                int idpersona = entrada.nextInt();
-                System.out.println("Ingresa el nombre del dormidito :");
+                //aqui mando a llamar al metodo para registrar a la persona
+                System.out.println("Ingresa el id de la persona: ");
+                int idpersona = Integer.parseInt(entrada.nextLine());
+                System.out.println("Ingresa el nombre de la persona: ");
                 String nombrepersona = entrada.nextLine();
-                System.out.println("Ingrese la edad del dormidito :");
+                System.out.println("Ingresa la edad de la persona: ");
                 int edadpersona = Integer.parseInt(entrada.nextLine());
-
-                //ahora debo de realizar una instancia de la persona 
-                Persona personaDormidita = new Persona(idpersona, nombrepersona, edadpersona);
-
-                control.agregarPersona(personaDormidita);
-                menu();
+                
+                //enviar esos datos al objeto
+                Persona personaNueva = new Persona(idpersona, nombrepersona, edadpersona);
+                
+                //con el metodo agregarlos
+                control.agregarPersona(personaNueva);
                 break;
             case 3:
-                //primero debo de buscar a la persona que quiero editar, por medio del id
-                System.out.println("Ingresa el id del dormidito a buscar");
-                idpersona = Integer.parseInt(entrada.nextLine());
-
-                //mando a llamar al metodo de busqueda
-                Persona personabuscar = control.buscarPersona(idpersona);
-
-                //veo la informacion
-                System.out.println("La informacion del dormidito es: "+ "\n" 
-                                + "ID :" + personabuscar.getId() + "\n"
-                                + "Nombre :" + personabuscar.getNombre() + "\n"
-                                + "Edad :" + personabuscar.getEdad() + "\n");
+                //aqui primero busco por el id y luego la edito
+                System.out.println("Ingrese el ID de la persona que desea actualizar sus datos");
+                int id = entrada.nextInt();
                 
-                //cambio los datos que desee
-                System.out.println("Ingresa el nuevo nombre del dormidito: ");
+                Persona personaSeleccione = control.buscarPersona(id);
+                
+                //muestrame los datos de esa persona
+                System.out.println("La informacion de la persona es la siguiente: ");
+                System.out.println("Id: " + personaSeleccione.getId()
+                                   + "Nombre: " + personaSeleccione.getNombre()
+                                   + "Edad: " + personaSeleccione.getEdad());
+                
+                //cuales son los cambios a realizar
+                
+                System.out.println("Ingrese el nuevo nombre del registro");
                 String nuevonombre = entrada.nextLine();
-                System.out.println("Ingresa la nueva edad del dormidito: ");
-                int nuevaedad = Integer.parseInt(entrada.nextLine());
-
-                //al tener los nuevos datos debo enviarlos
-
-                personabuscar.setNombre(nuevonombre);
-                personabuscar.setEdad(nuevaedad);
-
-                //ahora si actualizo el registro
-                control.actualizarPersona(personabuscar);
-                menu();
+                System.out.println("Ingrese la nueva edad del registro");
+                int edadnueva = entrada.nextInt();
+                
+                //tengo que enviar esos nuevos datos
+                
+                personaSeleccione.setNombre(nuevonombre);
+                personaSeleccione.setEdad(edadnueva);
+                
+                //ahora mandar a llamar a mi metodo actualizar
+                control.actualizarPersona(personaSeleccione);
+                
                 break;
             case 4:
-                System.out.println("Ingresa el id del dormidito a despertar");
-                idpersona = Integer.parseInt(entrada.nextLine());
-
-                //creo la persona para eliminarla
-                Persona personaEliminar = control.buscarPersona(idpersona);
-
-                //lo elimino
+                //primero busco por el id y luego la borro
+                System.out.println("Ingrese el ID de la persona que desea actualizar sus datos");
+                int ideliminar = entrada.nextInt();
+                
+                
+                //buscar primero no se nos olvide :3
+                
+                
+                Persona personaEliminar = control.buscarPersona(ideliminar);
+                
                 control.eliminarPersona(personaEliminar);
-
-                System.out.println("El dormidito ya desperto");
-                menu();
+                
+                System.out.println("Registro eliminado exitosamente");
+                
                 break;
             default:
-                System.out.println("Opcion no valida");
-                break;
-
-
+                System.out.println("Digite una opcion correcta");
         }
+        }while(opcion !=5);
     }
 }
